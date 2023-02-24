320db5cd45e6560824641bbad8537fc1f1e01b2c
Derrick <deitboy1992@gmail.com>
2023-02-23 19:34:53 -0500

    add logout to react front end

--- social-media-react/src/components/Navbar.jsx
+++ social-media-react/src/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "./Layout";
 
 import { Navbar, Container, Image, NavDropdown, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { getUser, useUserActions } from "../hooks/user.actions";
 
 function NavigationBar() {
+  const { setToaster } = useContext(Context);
+
   const userActions = useUserActions();
 
   const user = getUser();
-
+  const handleLogout = () => {
+    userActions.logout().catch((e) => {
+      setToaster({
+        type: "danger",
+        message: "Logout failed",
+        show: true,
+        title: e.data?.detail | "An error occurred.",
+      });
+    });
+  };
   return (
     <Navbar bg="primary" variant="dark">
       <Container>
@@ -25,9 +37,7 @@ function NavigationBar() {
               <NavDropdown.Item as={Link} to={`/profile/${user.id}/`}>
                 Profile
               </NavDropdown.Item>
-              <NavDropdown.Item onClick={userActions.logout}>
-                Logout
-              </NavDropdown.Item>
+              <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
